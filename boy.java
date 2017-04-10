20a68bc2033c80e4604f321a03161eb813eb2c99
Nimish Dusad <iit2015509@iiita.ac.in>
2017-04-10 08:03:38 +0530

    Add files via upload

---
+++ boy.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ * @author Nimish
+ * Boy class defining the boys
+ */
+public abstract class boy{
+
+	String name;
+	int attract;
+	int intellect;
+	public int budget;
+	int requirement;
+	String status;
+	String gf;
+	double happiness;
+	String type;
+
+	//Constructor for the boy class.
+
+	boy(String name,int attract,int intellect,int budget,int requirement,String status,String type){
+
+		this.name = name;
+		this.attract = attract;
+		this.intellect = intellect;
+		this.budget = budget;
+		this.requirement = requirement;
+		this.status = status;
+		this.gf = "";
+		this.type = type;
+	
+	}
+
+	//Matches the guy with his perfect one :D
+
+	boolean match(girl g){
+		if(this.budget >=g.expense && this.requirement <= g.attract){
+			return true;
+
+		}
+		else{
+			return false;
+		}
+	}
+	
+	abstract void calc_happiness(double value);
+
+
+	}
+
+
